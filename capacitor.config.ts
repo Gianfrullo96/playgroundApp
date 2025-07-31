210363676a0b00fd9c4c653648135d0e82cf2d31
Ferruzzi Gianluca <gianlucaferruzzi96@gmail.com>
2025-07-31 15:11:29 +0200

    GF-0009: android init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myliege.playgroundapp',
+  appName: 'playgroundApp',
+  webDir: 'dist/playground-app',
+};
+
+export default config;
